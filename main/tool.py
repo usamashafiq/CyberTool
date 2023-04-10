0506211559ac67757a96ecc1e7b41e3d78fbd5e1
usamashafiq <usamashafiq475@gmail.com>
2023-04-11 00:02:40 +0500

    Change the list

--- main/tool.py
+++ main/tool.py
@@ -1,9 +1,12 @@
 from main.tools import *
 import os
+
+
 def exit_program():
-        os.system("clear")
-        banner.main()
-        print("\033[38;5;105m","[+] Thanks visit again".title())
+    os.system("clear")
+    banner.main()
+    print("\033[38;5;105m", "[+] Thanks visit again".title())
+
 
 def main():
     while True:
@@ -12,10 +15,13 @@ def main():
         banner.attack("TOOLS")
         # import information_gathering
 
-        list_attacks=["Information Gathering","Vulnerability Analysis","Web Application Analysis","Password Attacks","Wireless Attacks","Exploitation Tools","Sniffing and Spoofing","Post Exploitation","Pentesting and Bug-Bounty","go back"]
-        #for output with index
+        list_attacks = ["Information Gathering",
+                        "Vulnerability Analysis", "Web Application Analysis",
+                        "Exploitation Tools", "Post Exploitation",
+                        "go back"]
+        # for output with index
         for i in range(len(list_attacks)):
-                print(colors.options,f"{i}) {list_attacks[i]}".title(),colors.reset)
+            print(colors.options, f"{i}) {list_attacks[i]}".title(), colors.reset)
         try:
             option = input(f"\n {colors.select}Select An Option ->{colors.reset}  ")
         except KeyboardInterrupt:
@@ -33,32 +39,18 @@ def main():
             os.system("clear")
             WEB_Application_Analysis.main()
         elif option == "3":
-            print("\n[+] Password Attacks")
-            os.system("clear")
-            Password_Hacking.main()
-        elif option == "4":
-            print("\n[+] Wireless Attacks")
-            os.system("clear")
-            Wireless_Hacking.main()
-        elif option == "5":
             print("\n[+] Exploitation Tools")
             os.system("clear")
             Exploitation_Tools.main()
-        elif option == "6":
-            print("\n[+] Sniffing and Spoofing")
-            os.system("clear")
-            Sniffing_and_Spoofing.main()
-        elif option == "7":
+        elif option == "4":
             print("\n[+] Post Exploitation")
             os.system("clear")
             PostExploitationAttacks.main()
-        elif option == "8":
-            print("\n[+] Pentesting and Bug-Bounty")
-            os.system("clear")
-            Pentesting_Bug_Bounty.main()
+
         else:
             exit_program()
             return
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     main()
